f13ba670cc8f373ffe0c0c636011babd9fce7862
hydref <charles.dutheil@gmail.com>
2019-03-12 14:55:03 +0100

    add validator in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
+  validates :vibe, inclusion: { in: %w(hip_hop classic exotique lounge electronique pop) } 
 end
