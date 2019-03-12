91f85f47f5b073e05ac180d2fd97ae7ed81a72a6
Karel2 <cloareckarel@gmail.com>
2019-03-12 12:09:40 +0100

    Add resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'places/index'
-  get 'places/new'
-  get 'places/create'
-  get 'places/update'
   devise_for :users
   root 'home#index'
   get 'dashboard', to: "dashboard#index"
+  resources :places
+
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
