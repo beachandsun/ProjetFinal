260b642d00c52b207b865a40d241b61fc50da47f
hydref <charles.dutheil@gmail.com>
2019-03-12 15:09:36 +0100

    add controller

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get users_create_url
+    assert_response :success
+  end
+
+  test "should get read" do
+    get users_read_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get users_update_url
+    assert_response :success
+  end
+
+  test "should get delete" do
+    get users_delete_url
+    assert_response :success
+  end
+
+end
