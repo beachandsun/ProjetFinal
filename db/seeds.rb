c71c8196ebbaec5ea822cdf4bf4ee9550137b898
Emma Calvet <emma.calvet.g@gmail.com>
2019-03-14 17:23:42 +0100

    update seed with tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,16 +17,18 @@ puts "Create addresses"
 10.times do
 	address = Address.create(address_number: Faker::Address.building_number, address_main: Faker::Address.street_name, city: "Montpellier", postal_code: "34000", country: "France") 
 end
+add = Address.create(address_number: "202", address_main: "rue de la croix du sud", city: "Montpellier", postal_code: "34000", country: "France")
 puts "Adresses created"
 
 puts "Create users"
 5.times do
-	user = User.create(email: Faker::Internet.free_email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "motdepasse", wifi: [true, false].sample, price: [true, false].sample, e_outlet: [true, false].sample, access_handi: [true, false].sample, vibe: ["exotique", "pop", "electronique" "hip_hop", "classic", "lounge"].sample, vegan: [true, false].sample, address_id: rand(Address.all.first.id..Address.all.last.id))
+	user = User.create(email: Faker::Internet.free_email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "motdepasse", wifi: [true, false].sample, price: [true, false].sample, e_outlet: [true, false].sample, access_handi: [true, false].sample, vibe: ["exotique", "pop", "electronique", "hip_hop", "classic", "lounge"].sample, vegan: [true, false].sample, address_id: rand(Address.all.first.id..Address.all.last.id))
 	end
 puts "Users created"	
 
 puts "Create places"
 5.times do
-		place = Place.create(name: Faker::Artist.name,description: Faker::Movies::HarryPotter.quote, phone_number: 456748394, wifi: [true, false].sample, price: [true, false].sample, e_outlet: [true, false].sample, access_handi: [true, false].sample, vibe: ["hip_hop", "classic", "exotique", "lounge", "electronique", "pop"].sample, vegan: [true, false].sample, address_id: rand(Address.all.first.id..Address.all.last.id))
+		place = Place.create(name: Faker::Coffee.blend_name, description: Faker::Movies::HarryPotter.quote, phone_number: 456748394, wifi: [true, false].sample, price: [true, false].sample, e_outlet: [true, false].sample, access_handi: [true, false].sample, vibe: ["hip_hop", "classic", "exotique", "lounge", "electronique", "pop"].sample, vegan: [true, false].sample, address_id: rand(Address.all.first.id..Address.all.last.id))
 	end
-puts "Places created"		
+	place2 = Place.create(name: Faker::Coffee.blend_name, description: Faker::Movies::HarryPotter.quote, phone_number: 456748394, wifi: [true, false].sample, price: [true, false].sample, e_outlet: [true, false].sample, access_handi: [true, false].sample, vibe: ["hip_hop", "classic", "exotique", "lounge", "electronique", "pop"].sample, vegan: [true, false].sample, address_id: Address.all.last.id)
+puts "Places created"
