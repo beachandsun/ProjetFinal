e98d95daaa5912fa67412c9c9a11847d4d037b8d
hydref <charles.dutheil@gmail.com>
2019-03-20 16:26:57 +0100

    fix issu validate city

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,11 +5,11 @@ class Address < ApplicationRecord
 	validates :address_main, presence: true
 	validates :city, presence: true
 	validates :postal_code, presence: true
-	validates :country, presence: true
+	# validates :country, presence: true
 
     validates :postal_code, :length => { :minimum => 5, :maximum => 5 }, :format => { with: /[0-9]+/ }
-    validates :country, :format => { with: /\A[a-zA-Z]+\z/ }
-    validates :city, :format => { with: /\A[a-zA-Z]+\z/ }
+    # validates :country, :format => { with: /\A[a-zA-Z]+\z/ }
+    validates :city, :format => { with: /\A[a-zA-Z -]+\z/ }
     validates :address_number, :format => { with: /\A[0-9]+\z/ }
 
 
