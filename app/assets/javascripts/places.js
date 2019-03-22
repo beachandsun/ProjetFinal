338c3e14a43edf15b321187df8cfa03abe873f03
Emma Calvet <emma.calvet.g@gmail.com>
2019-03-22 19:14:10 +0100

    fix for heroku

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -1,12 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-$(document).ready(function () {
-
-    // MODAL ONLOAD
-    j$(window).on(‘load’, function() {
-        $(‘#myModal’).modal(‘show’);
-    });
-
-
-});
